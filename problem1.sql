dd16dc7952ee001317baccea04d7d33b40a18e07
abdulkarim-hamidi <abdulkarim.hamidi@yahoo.com>
2023-02-22 23:12:56 +0000

    lab completd

---
+++ problem1.sql
@@ -0,0 +1,3 @@
+insert into song(title, artist) values ('Let it be', 'Beatles'), 
+                        ('Hotel California', 'Eagles'),
+                        ('Kashmir', 'Led Zeppelin');
--- src/main/java/InsertARecord.java
+++ src/main/java/InsertARecord.java
@@ -9,22 +9,28 @@ import java.sql.Statement;
 /**
  * SQL sublanguage: DML (Data Manipulation Language)
  *
- * Now that we can create and drop tables, we need to be able to insert records into the table.
+ * Now that we can create and drop tables, we need to be able to insert 
+ * records into the table.
  *
  * The syntax for inserting a record is as follows:
- *  INSERT INTO table_name (col_1, col_2, ...col_N) VALUES (val_1, val_2, ...val_N);
+ *  INSERT INTO table_name (col_1, col_2, ...col_N) VALUES 
+ * (val_1, val_2, ...val_N);
  *
  *  in the above statement:
  *      - "col_1, col_2, ...col_N"  is referring to the column name of the table.
- *      - "val_1, val_2, ...val_N" is referring to the data we are trying to insert into that specific column.
+ *      - "val_1, val_2, ...val_N" is referring to the data we are trying to 
+ * insert into that specific column.
  *
- * Additional reference material if needed: https://www.w3schools.com/sql/sql_insert.asp
+ * Additional reference material if needed: 
+ * https://www.w3schools.com/sql/sql_insert.asp
  * */
 public class InsertARecord {
 
     /**
-     * Activity: Assuming the table "song" already exists, write the sql statement in the problem1.sql file that allows
-     * us to add a new record into the table "song". It should work with any values for title and artist!
+     * Activity: Assuming the table "song" already exists, write the sql 
+     * statement in the problem1.sql file that allows
+     * us to add a new record into the table "song". It should work with any 
+     * values for title and artist!
      *
      *          song Table Diagram:
      *          |      title        |        artist         |
@@ -33,7 +39,8 @@ public class InsertARecord {
      *          |'Hotel California' |'Eagles'               |
      *          |'Kashmir'          |'Led Zeppelin'         |
      *
-     * Do not change anything in this code. You should write your sql statement on a single line in the
+     * Do not change anything in this code. You should write your sql 
+     * statement on a single line in the
      * problem1.sql file.
      * */
     public boolean problem1(){
